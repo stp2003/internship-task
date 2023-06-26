7bcacd7a5469ee9ee208617e481f90ab8f94158c
stp2003 <100586951+stp2003@users.noreply.github.com>
2023-06-26 21:45:59 +0530

    initial home screen made

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:internship_task/screens/detail_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({Key? key}) : super(key: key);
@@ -147,11 +148,11 @@ class HomeScreen extends StatelessWidget {
                               index == 0
                                   ? Icons.home
                                   : index == 1
-                                      ? Icons.place
+                                      ? Icons.place_outlined
                                       : index == 2
-                                          ? Icons.flight
-                                          : Icons.fastfood,
-                              color: index == 0 ? Colors.white : Colors.black38,
+                                          ? Icons.flight_outlined
+                                          : Icons.fastfood_outlined,
+                              color: index == 0 ? Colors.white : Colors.black54,
                               size: 32.0,
                             ),
                             Text(
@@ -283,7 +284,7 @@ class HomeScreen extends StatelessWidget {
                                 ),
                               ),
                             ],
-                          )
+                          ),
                         ],
                       ),
                     );
@@ -307,124 +308,149 @@ class HomeScreen extends StatelessWidget {
                 ),
               ),
               const SizedBox(height: 15.0),
-              SizedBox(
-                height: 180.0,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  shrinkWrap: true,
-                  itemCount: hotelName.length,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      width: 360.0,
-                      margin: const EdgeInsets.symmetric(horizontal: 12.0),
-                      padding: const EdgeInsets.all(8.0),
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: NetworkImage(
-                            hotHotelLink[index],
-                          ),
-                          fit: BoxFit.cover,
-                        ),
-                        borderRadius: BorderRadius.circular(15.0),
-                        border: Border.all(color: Colors.transparent),
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+              InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const DetailScreen(),
+                    ),
+                  );
+                },
+                child: SizedBox(
+                  height: 180.0,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    shrinkWrap: true,
+                    itemCount: hotelName.length,
+                    itemBuilder: (context, index) {
+                      return Stack(
                         children: [
-                          ElevatedButton(
-                            onPressed: () {},
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: const Color(0xFFE5670C),
-                            ),
-                            child: Text(
-                              '${hotHotelDiscount[index]}% OFF',
-                              style: const TextStyle(
-                                fontFamily: 'poppins_medium',
-                                color: Colors.white,
+                          Positioned(
+                            left: 10.8,
+                            right: 10.3,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(15.0),
+                              child: Image.network(
+                                height: 180.0,
+                                width: 360.0,
+                                fit: BoxFit.cover,
+                                hotHotelLink[index],
                               ),
                             ),
                           ),
-                          const SizedBox(height: 45.0),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                hotHotelName[index],
-                                style: const TextStyle(
-                                  fontSize: 18.0,
-                                  fontFamily: "poppins_bold",
-                                  color: Colors.white,
+                          Container(
+                            width: 325.0,
+                            margin:
+                                const EdgeInsets.symmetric(horizontal: 12.0),
+                            padding: const EdgeInsets.all(8.0),
+                            decoration: BoxDecoration(
+                              color: Colors.black26,
+                              borderRadius: BorderRadius.circular(15.0),
+                              border: Border.all(color: Colors.transparent),
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                ElevatedButton(
+                                  onPressed: () {},
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: const Color(0xFFE5670C),
+                                  ),
+                                  child: Text(
+                                    '${hotHotelDiscount[index]}% OFF',
+                                    style: const TextStyle(
+                                      fontFamily: 'poppins_medium',
+                                      color: Colors.white,
+                                    ),
+                                  ),
                                 ),
-                              ),
-                              // const SizedBox(width: 100.0),
-                              Row(
-                                children: [
-                                  const Icon(
-                                    Icons.star,
-                                    color: CupertinoColors.systemYellow,
-                                    size: 15.0,
-                                  ),
-                                  const SizedBox(width: 4.0),
-                                  Text(
-                                    hotHotelRatings[index],
-                                    style: const TextStyle(
-                                      fontSize: 14.0,
-                                      fontFamily: "poppins",
-                                      color: Colors.white,
+                                const SizedBox(height: 45.0),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      hotHotelName[index],
+                                      style: const TextStyle(
+                                        fontSize: 18.0,
+                                        fontFamily: "poppins_bold",
+                                        color: Colors.white,
+                                      ),
                                     ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                          const SizedBox(height: 10.0),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Row(
-                                children: [
-                                  const Icon(
-                                    Icons.place_outlined,
-                                    color: Colors.white,
-                                    size: 17.0,
-                                  ),
-                                  const SizedBox(width: 5.0),
-                                  Text(
-                                    hotHotelLocation[index],
-                                    style: const TextStyle(
-                                      fontSize: 16.0,
-                                      fontFamily: "poppins",
-                                      color: Colors.white,
+                                    // const SizedBox(width: 100.0),
+                                    Row(
+                                      children: [
+                                        const Icon(
+                                          Icons.star,
+                                          color: CupertinoColors.systemYellow,
+                                          size: 15.0,
+                                        ),
+                                        const SizedBox(width: 4.0),
+                                        Text(
+                                          hotHotelRatings[index],
+                                          style: const TextStyle(
+                                            fontSize: 14.0,
+                                            fontFamily: "poppins_medium",
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ],
                                     ),
-                                  ),
-                                ],
-                              ),
-                              Row(
-                                children: [
-                                  Text(
-                                    '₹${hotHotelAmount[index]}',
-                                    style: const TextStyle(
-                                      fontSize: 16.0,
-                                      fontFamily: "poppins_bold",
-                                      color: Colors.white,
+                                  ],
+                                ),
+                                const SizedBox(height: 10.0),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Row(
+                                      children: [
+                                        const Icon(
+                                          Icons.place_outlined,
+                                          color: Colors.white,
+                                          size: 17.0,
+                                        ),
+                                        const SizedBox(width: 5.0),
+                                        Text(
+                                          hotHotelLocation[index],
+                                          style: const TextStyle(
+                                            fontSize: 16.0,
+                                            fontFamily: "poppins",
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ],
                                     ),
-                                  ),
-                                  const Text(
-                                    '/night',
-                                    style: TextStyle(
-                                      fontSize: 14.0,
-                                      fontFamily: "poppins",
-                                      color: Colors.white,
+                                    Row(
+                                      children: [
+                                        Text(
+                                          '₹${hotHotelAmount[index]}',
+                                          style: const TextStyle(
+                                            fontSize: 16.0,
+                                            fontFamily: "poppins_bold",
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                        const Text(
+                                          '/night',
+                                          style: TextStyle(
+                                            fontSize: 14.0,
+                                            fontFamily: "poppins",
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ],
                                     ),
-                                  ),
-                                ],
-                              ),
-                            ],
+                                  ],
+                                ),
+                              ],
+                            ),
                           ),
                         ],
-                      ),
-                    );
-                  },
+                      );
+                    },
+                  ),
                 ),
               ),
             ],
