1ae918bcab5ccf9b2fbef52e85ae8e4307effd73
stp2003 <100586951+stp2003@users.noreply.github.com>
2023-06-28 15:22:08 +0530

    initial detail screen made

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:internship_task/screens/home_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
   runApp(const MyApp());
 }
 
