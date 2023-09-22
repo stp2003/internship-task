95693704941cd61689e4e8c898afa7ebd155a24b
Shashwat <100586951+stp2003@users.noreply.github.com>
2023-09-22 17:38:55 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'
 import 'package:flutter/services.dart';
 import 'package:internship_task/screens/home_screen.dart';
 
